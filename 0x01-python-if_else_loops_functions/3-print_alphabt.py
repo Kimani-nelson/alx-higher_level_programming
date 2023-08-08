26af33418693ad93239b045f0e4f66bb960749a4
teargas001 <nkimnjoro@gmail.com>
2023-08-08 15:45:22 -0700

    mod

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-
-for c in range(ord('a'), ord('z') + 1):
-    if c != ord('q') and c != ord('e'):
-        print(chr(c), end='')
-print()  # To add a newline at the end
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
