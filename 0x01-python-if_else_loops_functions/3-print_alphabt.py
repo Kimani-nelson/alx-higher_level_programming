0751fafbf25f65131e45db81ca708e0983310c90
teargas001 <nkimnjoro@gmail.com>
2023-08-08 15:42:27 -0700

    mod

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-for i in range(97, 123):
-    if (i == 101) or (i == 113):
-        continue
-    print(chr(i).format(), end="")
+
+for c in range(ord('a'), ord('z') + 1):
+    if c != ord('q') and c != ord('e'):
+        print(chr(c), end='')
