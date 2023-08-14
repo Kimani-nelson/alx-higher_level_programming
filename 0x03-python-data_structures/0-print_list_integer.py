65372d67d938e0900596121133e9aa6c09d2d59a
teargas001 <nkimnjoro@gmail.com>
2023-08-14 02:10:35 -0700

    Correction

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 
 def print_list_integer(my_list=[]):
-    for num in my_list:
-        print("{:d}".format(num))
+    for number in my_list:
+        print("{:d}".format(number))
 
-# Test the function with an example list
-example_list = [1, 2, 3, 4, 5]
-print_list_integer(example_list)
+# Example usage
+if __name__ == "__main__":
+    my_list = [1, 2, 3, 4, 5]
+    print_list_integer(my_list)
