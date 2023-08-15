2d62543dfb621277f31e286c371e3b76a863d3e7
teargas001 <nkimnjoro@gmail.com>
2023-08-15 06:16:26 -0700

    Revised

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -5,11 +5,3 @@ def replace_in_list(my_list, idx, element):
         return my_list
     my_list[idx] = element
     return my_list
-
-# Example usage
-if __name__ == "__main__":
-    my_list = [10, 20, 30, 40, 50]
-    index = 2
-    new_element = 35
-    result = replace_in_list(my_list, index, new_element)
-    print("Updated list:", result)
