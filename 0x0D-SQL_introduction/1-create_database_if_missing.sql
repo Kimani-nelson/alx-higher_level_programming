cef969d3101868376cf037ee6fd71b694aa2af7a
teargas001 <nkimnjoro@gmail.com>
2023-10-17 19:06:43 -0700

    sql

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,22 +1,3 @@
-#!/bin/bash
-
-# MySQL server information
-MYSQL_USER="your_username"
-MYSQL_PASSWORD="your_password"
-MYSQL_HOST="localhost"
-
-# Database name to create
-DB_NAME="hbtn_0c_0"
-
-# Check if the database already exists
-EXISTING_DB=$(mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -h "$MYSQL_HOST" -e "SELECT SCHEMA_NAME FROM INFORMATION_SCHEMA.SCHEMATA WHERE SCHEMA_NAME = '$DB_NAME';" --skip-column-names)
-
-if [ -z "$EXISTING_DB" ]; then
-    # The database doesn't exist, so create it
-    mysql -u "$MYSQL_USER" -p"$MYSQL_PASSWORD" -h "$MYSQL_HOST" -e "CREATE DATABASE $DB_NAME;"
-    echo "Database $DB_NAME created successfully."
-else
-    # The database already exists
-    echo "Database $DB_NAME already exists."
-fi
+-- Creates the database hbtn_0c_0 in my MySQL server.
+CREATE DATABASE IF NOT EXISTS `hbtn_0c_0`;
 
