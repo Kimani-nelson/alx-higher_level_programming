17777677280f3075a5e5517d0a5b3cabd0e18eae
teargas001 <nkimnjoro@gmail.com>
2023-11-30 02:53:14 -0800

    Body

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
-# send a GET request to an URL with curl, and display the body of the response
-curl -sL "$1"
+# This script takes in a URL, sends a GET request to the URL using curl,
+# and displays the body of the response if the response status code is 200
+
+URL=$1
+STATUS_CODE=$(curl -s -o /dev/null -w "%{http_code}" "$URL" 2>/dev/null)
+
+if [ "$STATUS_CODE" -eq 200 ]; then
+    curl -s "$URL"
+elif [ "$STATUS_CODE" -ge 400 ]; then
+    echo "Error: HTTP $STATUS_CODE"
+elif [ "$STATUS_CODE" = "000" ]; then
+    echo "Error: Unexpected response code $STATUS_CODE"
+else
+    echo "Unexpected response: HTTP $STATUS_CODE"
+fi
 
