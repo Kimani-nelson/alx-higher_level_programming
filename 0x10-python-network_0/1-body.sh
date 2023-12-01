cca90747fcd16f4a023558091e37ac1e57180f94
teargas001 <nkimnjoro@gmail.com>
2023-11-30 18:04:58 -0800

    update of cURL to the end

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-# Sends a GET request to the given URL and displays the body of a 200 status code response
-curl -sL -w "%{http_code}" -o /dev/null "$1" | {
-    read -r status
-    if [ "$status" -eq 200 ]; then
-        curl -s "$1"
-    fi
-}
+#  takes in a URL, sends a GET request to the URL, and displays the body of the response
+curl -sL "$1"
